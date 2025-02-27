f37b5a6a413a44458879e4567d800e3908e7ec0f
Davy Assunção <davy.assuncao@fretebras.com.br>
2025-02-27 16:48:08 -0300

    test: ensure return an account on success

--- src/infra/database/mongodb/repositories/account/account.repository.ts
+++ src/infra/database/mongodb/repositories/account/account.repository.ts
@@ -1,10 +1,10 @@
 import { AddAccountRepository } from "../../../../../domain/protocols/add-account.protocol";
-import { AddAccountModelDTO } from "../../../../../domain/dtos/add-account.dto";
+import { AddAccountDTO } from "../../../../../domain/dtos/add-account.dto";
 import { AccountModel } from "../../../../../domain/models/account.model";
 import { User } from "../../schemas/user.schema";
 
 export class AccountRepository implements AddAccountRepository {
-  async add(data: AddAccountModelDTO): Promise<AccountModel> {
+  async add(data: AddAccountDTO): Promise<AccountModel> {
     const user = await User.create(data);
     return {
       id: user._id.toHexString(),
