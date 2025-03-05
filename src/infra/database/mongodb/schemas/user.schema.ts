ba16b5aad6f754d611e8a4c411ef3a6a7695383c
Davy Assunção <davy.assuncao@fretebras.com.br>
2025-03-05 12:20:42 -0300

    chore: add timestamp filds

--- src/infra/database/mongodb/schemas/user.schema.ts
+++ src/infra/database/mongodb/schemas/user.schema.ts
@@ -16,6 +16,14 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export const User = mongoose.model("User", UserSchema);
